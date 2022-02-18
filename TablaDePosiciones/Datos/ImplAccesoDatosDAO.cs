ecb6fd0ee82d2fe73d3cff1ac77a5af70c0adfe8
Daniela Castillo <lcastillog17@miumg.edu.gt>
2022-02-17 22:44:12 -0600

    Escribir Json

--- TablaDePosiciones/Datos/ImplAccesoDatosDAO.cs
+++ TablaDePosiciones/Datos/ImplAccesoDatosDAO.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TablaDePosiciones.Datos
 {
-     class ImplAccesoDatosDAO : InterfazAccesoDatos
+
+    class ImplAccesoDatosDAO : InterfazAccesoDatos
     {
-        public bool escribirDatos(string[,] matriz)
+
+
+
+        public void escribirDatos(string[,] matriz)
         {
-            throw new NotImplementedException();
+            File.WriteAllText(_path, SerializarJsonFile(matriz));
         }
 
         public string[,] leerDatos()
         {
-            throw new NotImplementedException();
+            
+            var leido = obtenerMatrizDeJason;
+            return desserializarJsonFile(leido);
+        }
+
+        private static string _path = @"../archivo.json";
+        public static string SerializarJsonFile(string[,] matriz)
+        {
+            string matrizJson = JsonConvert.SerializeObject(matriz, Formatting.Indented);
+           return matrizJson;
+        }
+
+        public static String obtenerMatrizDeJason()
+        {
+            string MatrizDeJason;
+            /*el reader es para leer un archivo de disco*/ using (var reader = new StreamReader(_path))
+            {
+                MatrizDeJason = reader.ReadToEnd();
+            }
+            return MatrizDeJason;
+        }
+
+        public static void desserializarJsonFile(string MatrizDeJason)
+        {
+            Console.WriteLine(MatrizDeJason);
+            var leido = JsonConvert.DeserializeObject<List<>>(MatrizDeJason) ;
         }
     }
 }
